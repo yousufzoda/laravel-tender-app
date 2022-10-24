4055a024d38e0a15bdb606003d7486d3457bc233
Yousufzoda Komron <komron.yusufjonovich@gmail.com>
2022-10-24 23:20:14 +0300

    Create TenderResource

---
+++ app/Http/Resources/TenderResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TenderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'external_code' => $this->external_code,
+            'number' => $this->number,
+            'status' => $this->status,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
